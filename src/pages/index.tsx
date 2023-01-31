2b69c0253850820435d9b151031a1b5098c86b21
Derrint <lorensioderrint@gmail.com>
2023-01-31 08:06:48 +0700

    feat: update home content, add animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { FiGithub, FiInstagram, FiLinkedin, FiTwitter } from 'react-icons/fi';
+import Fade from 'react-reveal/Fade';
 
 import { Meta } from '@/layouts/Meta';
 import { Tokyo } from '@/templates/Tokyo';
@@ -42,52 +43,58 @@ const Index = () => {
         />
       }
     >
-      {/* HOME */}
-      <div id="home" className="tokyo_tm_section animated">
-        <div className="container">
-          <div className="tokyo_tm_home relative clear-both flex min-h-[100vh] w-full items-center justify-center">
-            <div className="home_content flex items-center">
-              <div
-                className="avatar relative min-h-[300px] min-w-[300px] rounded-full"
-                data-type="wave"
-              >
+      <Fade cascade>
+        {/* HOME */}
+        <div id="home" className="tokyo_tm_section animated">
+          <div className="container">
+            <div className="tokyo_tm_home relative clear-both flex min-h-[100vh] w-full items-center justify-center">
+              <div className="home_content flex items-center">
                 <div
-                  className="image absolute inset-0 bg-cover bg-center bg-no-repeat"
-                  style={{
-                    backgroundImage: `url('/assets/img/cv/foto-derrint.jpg')`,
-                  }}
-                />
-              </div>
-              <div className="details ml-[80px]">
-                <h3 className="name mb-[14px] text-[55px] font-extrabold uppercase">
-                  Lorensio <span>Derrint</span>
-                </h3>
-                <p className="job mb-[25px] max-w-[450px] font-montserrat font-medium">
-                  I&apos;m a creative Software Engineer with a great willingness
-                  to learn and master Front-End & Mobile Development.
-                </p>
-                <div className="social float-left w-full">
-                  <ul className="m-0 list-none">
-                    {socials.map(({ id, url, icon }) => (
-                      <li key={id} className="mr-3 inline-block">
-                        <a
-                          className="text-[20px] text-black transition-all duration-300 hover:text-black"
-                          href={url}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          {icon}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
+                  className="avatar relative min-h-[300px] min-w-[300px] rounded-full"
+                  data-type="wave"
+                >
+                  <div
+                    className="image absolute inset-0 bg-cover bg-center bg-no-repeat"
+                    style={{
+                      backgroundImage: `url('/assets/img/cv/foto-derrint.jpg')`,
+                    }}
+                  />
+                </div>
+
+                <div className="details ml-[80px]">
+                  <h3 className="name mb-[14px] text-[55px] font-extrabold uppercase">
+                    <Fade left cascade duration={500} delay={250}>
+                      Lorensio Derrint
+                    </Fade>
+                  </h3>
+                  <p className="job mb-[25px] max-w-[450px] font-montserrat font-medium">
+                    I&apos;m a creative Software Engineer with a great
+                    willingness to learn and master Front-End & Mobile
+                    Development.
+                  </p>
+                  <div className="social float-left w-full">
+                    <ul className="m-0 list-none">
+                      {socials.map(({ id, url, icon }) => (
+                        <li key={id} className="mr-3 inline-block">
+                          <a
+                            className="text-[20px] text-black transition-all duration-300 hover:text-black"
+                            href={url}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            {icon}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* /HOME */}
+        {/* /HOME */}
+      </Fade>
     </Tokyo>
   );
 };
